031ba787824a2649e211dec2b5639a4c17a0a037
Gustavo <bicalhogustavo866@gmail.com>
2024-09-02 10:07:45 -0300

    adicionando repositorio no git

---
+++ mapas.h
@@ -0,0 +1,4 @@
+// Declaração das funcoes
+void printGrid(int grid[9][9]);
+void retornaDificuldade();
+
---
+++ sudoku.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mapas.h"
+#include <time.h>
+
+#define SIZE 9
+
+int main() {
+
+    int dificuldade = 0;  
+    int preenchidos = 0;
+    int x, y, valor = 0;
+    
+
+
+    // crie o momento que o usuário seleciona a dificyuldade
+    retornaDificuldade();
+
+
+
+    // CriaMatriz(grid);
+    // printf("qual coordenada voce gostaria de preencher?: \n");
+    // scanf("%i %i", &x, &y);
+    // printf("com qual valor? \n");
+    // scanf("%i", &valor);
+    // grid[x][y] = valor;
+
+    return 0;
+}
